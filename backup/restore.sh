aa04d9c67a8ce233b5016a62e3c852bf2c8bef67
mirhec <mirhec@users.noreply.github.com>
2017-10-20 14:30:30 +0200

    Update restore.sh

--- backup/restore.sh
+++ backup/restore.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm --env-file backup.env -v /var/www:/data:rw dockerhosting/backup get --skip-existing
+docker run --rm --env-file backup.env -v /var/www:/data:rw dockerhosting/backup get
